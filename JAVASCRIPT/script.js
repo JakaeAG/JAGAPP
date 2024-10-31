28c80b7240e883132fc66e6cbc17e1d662ecd0b5
JakeAG <112973123+JakaeAG@users.noreply.github.com>
2024-10-31 15:04:08 -0400

    Update script.js

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -1,8 +1,8 @@
 function mostrarAñadirProd() {
-    window.location.href = 'HTML/añadirprod.html'; 
+    window.location.href = '../HTML/añadirprod.html'; 
 }
 function mostrarVerProd() {
-    window.location.href = 'HTML/ver_Productos.html'; 
+    window.location.href = '../HTML/ver_Productos.html'; 
 }
 
 let divContent = document.querySelector('col-sm-6 col-xs-6 centrado').innerHTML;
